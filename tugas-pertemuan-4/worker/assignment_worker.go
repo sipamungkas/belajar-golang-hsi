d490d892762b19306c50eb7a97282d7291a922c7
Ragil Burhanudin Pamungkas <sipamungkas.dev@gmail.com>
2025-08-03 15:06:07 +0700

    feat(tugas-pertemuan-4): assignment worker random tugas for mahasiswa

--- tugas-pertemuan-4/worker/assignment_worker.go
+++ tugas-pertemuan-4/worker/assignment_worker.go
@@ -2,27 +2,36 @@ package worker
 
 import (
 	"fmt"
+	"math/rand"
 	"tugas-pertemuan-4/models"
 
 	"gorm.io/gorm"
 )
 
+var taskList []string = []string{
+	"Tugas Pemrograman Goroutine",
+	"Tugas Implementasi WaitGroup",
+	"Tugas Implementasi Mutex",
+	"Tugas Implementasi Channel",
+	"Tugas Implementasi WaitGroup"}
+
+// func removeTask(s []string, index int) []string {
+// 	return append(s[:index], s[index+1:]...)
+// }
+
 func AssignmentWorker(db *gorm.DB, student models.Mahasiswa, msgChan chan<- string) {
 
-	taskList := []string{
-		"Tugas Pemrograman Goroutine",
-		"Tugas Implementasi WaitGroup",
-		"Tugas Implementasi Mutex",
-		"Tugas Implementasi Channel",
-		"Tugas Implementasi WaitGroup"}
+	taskCount := len(taskList)
+
+	randomIndex := rand.Intn(taskCount - 1)
 
 	db.Create(&models.Tugas{
-		Judul:       taskList[1],
-		Deskripsi:   taskList[1],
+		Judul:       taskList[randomIndex],
+		Deskripsi:   taskList[randomIndex],
 		MahasiswaId: student.ID,
 	})
 
-	message := fmt.Sprintf("Tugas '%s' diberikan ke %s", taskList[1], student.Nama)
+	message := fmt.Sprintf("Tugas '%s' diberikan ke %s", taskList[randomIndex], student.Nama)
 
 	msgChan <- message
 
